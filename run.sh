6dbc7f43c43dad6fb38229768029273d0d4c22cb
radiliadi <radiliadi@gmail.com>
2025-05-25 13:12:25 +0700

    fix: the mission one

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 
 source venv/bin/activate
 pip freeze > requirements-lock.txt
+mkdir -p src/generated && touch src/generated/sklearn_model.pkl
 python src/train_model.py
 PYTHONPATH=./src uvicorn main:app --reload --app-dir src --port 8000
